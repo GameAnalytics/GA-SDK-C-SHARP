0ee9c333cb4416de06d05ef503dff8deefa79a48
Martin Treacy-Schwartz <thetreacyschwartz@gmail.com>
2016-09-09 10:14:48 +0200

    initial travis setup added

---
+++ GA_SDK_MONO_TEST/GAValidatorTest.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using NUnit.Framework;
+using GameAnalyticsSDK.Net.Logging;
+using GameAnalyticsSDK.Net.Validators;
+
+namespace GameAnalyticsSDK.Net
+{
+	[TestFixture()]
+	public class GAValidatorTest
+	{
+		[Test()]
+		public void TestValidateCurrency()
+		{
+			GALogger.InfoLog = true;
+
+			Assert.True(GAValidator.ValidateCurrency("USD"));
+			Assert.True(GAValidator.ValidateCurrency("XXX"));
+
+			Assert.False(GAValidator.ValidateCurrency("usd"));
+			Assert.False(GAValidator.ValidateCurrency("US"));
+			Assert.False(GAValidator.ValidateCurrency("KR"));
+			Assert.False(GAValidator.ValidateCurrency("USDOLLARS"));
+			Assert.False(GAValidator.ValidateCurrency("$"));
+			Assert.False(GAValidator.ValidateCurrency(""));
+			Assert.False(GAValidator.ValidateCurrency(null));
+		}
+	}
+}
