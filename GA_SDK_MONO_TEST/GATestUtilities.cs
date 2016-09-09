0fb1620d6ba1b81a21d7c82ccfd15d6fda74a79c
Martin Treacy-Schwartz <thetreacyschwartz@gmail.com>
2016-09-09 11:20:42 +0200

    tests updated

---
+++ GA_SDK_MONO_TEST/GATestUtilities.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace GameAnalyticsSDK.Net
+{
+	public static class GATestUtilities
+	{
+		public static string GetRandomString(int numberOfCharacters)
+		{
+			const string letters = "abcdefghijklmfalsepqrstuvwxyzABCDEFGHIJKLMfalsePQRSTUVWXYZ0123456789";
+
+			Random rd = new Random();
+
+			string ret = "";
+
+			for(int i = 0; i < numberOfCharacters; i++)
+			{
+				ret += letters[rd.Next(letters.Length)];
+			}
+
+			return ret;
+		}
+	}
+}
