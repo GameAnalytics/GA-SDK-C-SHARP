0e2dc6bbc652da48758da78d10c021e3272262fa
Martin Treacy-Schwartz <thetreacyschwartz@gmail.com>
2016-11-19 00:50:44 +0100

    version updated

--- GA-SDK-MONO-SHARED/Device/GADevice.cs
+++ GA-SDK-MONO-SHARED/Device/GADevice.cs
@@ -20,11 +20,11 @@ namespace GameAnalyticsSDK.Net.Device
 	internal static class GADevice
 	{
 #if WINDOWS_UWP
-        private const string _sdkWrapperVersion = "uwp 1.1.2";
+        private const string _sdkWrapperVersion = "uwp 1.1.3";
 #elif WINDOWS_WSA
-        private const string _sdkWrapperVersion = "wsa 1.1.2";
+        private const string _sdkWrapperVersion = "wsa 1.1.3";
 #else
-        private const string _sdkWrapperVersion = "mono 1.1.2";
+        private const string _sdkWrapperVersion = "mono 1.1.3";
 #endif
 #if UNITY
 		private static readonly string _buildPlatform = UnityRuntimePlatformToString(Application.platform);
